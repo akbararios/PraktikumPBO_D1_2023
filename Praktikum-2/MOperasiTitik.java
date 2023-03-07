45cbd00f6d8bcf271f869a32e315013e276863a9
Akbar Ario Satrio U <90430654+akbararios@users.noreply.github.com>
2023-03-07 20:22:21 +0700

    Update MOperasiTitik.java

--- Praktikum-2/MOperasiTitik.java
+++ Praktikum-2/MOperasiTitik.java
@@ -1,3 +1,10 @@
+ /**
+ * File : OperasiTitik.java 7/03/23
+ * Penulis : Akbar Ario Satrio Utomo
+ * Deskripsi : kelas yang berisi program untuk menjalankan operasi pada titik
+ * 			
+ */ 
+
 public class MOperasiTitik{
 	public static void main(String[] args){
 		Titik t  = new Titik(4,4);
